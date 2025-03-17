059a3b0a10d5a9cddf9ab1c5d1bc79ebee7393ea
sonderson0 <danielrechozza@live.com>
2025-03-16 19:53:29 -0600

    Add files via upload

---
+++ arearectangulo.java
@@ -0,0 +1,23 @@
+/*calcular el area de un rectangulo */
+
+import java.util.Scanner;
+
+public class arearectangulo {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    float area,base,altura;
+    System.out.println("Ingresa la base del rectangulo");
+    base = input.nextFloat();
+    System.out.println("Ingresa la altura");
+    altura = input.nextFloat();
+    area = base * altura;
+    System.out.println("El area del rectangulo es: "+area+" cm2");
+    input.close();
+
+
+
+
+    
+
+    }
+}
